c965d9001cc62c2a433639505337de10a0ab9846
smart <32380846+ApiwanN@users.noreply.github.com>
2017-12-06 16:28:00 +0700

    ชิ้น3

---
+++ ICT13580013B3.Api/Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ICT13580013B3.Api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICT13580013B3.Api.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Product> Products
+        {
+            get;
+            set;
+        }
+        public MyDbContext(DbContextOptions<MyDbContext> options) :base(options)
+        {
+        }
+    }
+}
---
+++ ICT13580013B3/ICT13580013B3Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580013B3
+{
+    public partial class ICT13580013B3Page : ContentPage
+    {
+        public ICT13580013B3Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
